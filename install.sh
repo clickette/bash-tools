edaef90aa33f19b943aca483e2b078400bfff2d7
ClaytonTDM <claytontdm@gmail.com>
2022-10-21 21:10:14 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ cd /var/www/html
 sudo rm -rf *
 sudo rm -rf *.*
 cd ..
-sudo git pull https://github.com/clickette/main.git html
+sudo git clone https://github.com/clickette/main.git html
 sudo curl https://raw.githubusercontent.com/clickette/bash-tools/main/superpull.sh --output superpull.sh
 bash superpull.sh
