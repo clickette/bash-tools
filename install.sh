8a89cdd96892c97cfc03404e2a1c4552c1a37c7a
ClaytonTDM <claytontdm@gmail.com>
2022-10-27 15:10:55 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-sudo apt update
-sudo apt install nginx
+sudo apt-get update
+sudo apt-get install nginx
 cd /var/www/html
-sudo rm -rf *
-sudo rm -rf *.*
-cd ..
+sudo rm -rf /var/www/html/*
+sudo rm -rf /var/www/html/*.*
+cd /var/www
 sudo git clone https://github.com/clickette/main.git html
 sudo curl https://raw.githubusercontent.com/clickette/bash-tools/main/superpull.sh --output superpull.sh
 bash superpull.sh
